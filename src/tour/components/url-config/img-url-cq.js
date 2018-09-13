24cd7a9095ba01575a25774451bf44f2096dd2ad
Snake <zcfxs@hotmail.com>
2018-09-14 01:16:23 +0800

    build

--- src/tour/components/url-config/img-url-cq.js
+++ src/tour/components/url-config/img-url-cq.js
@@ -1,18 +1,18 @@
 const IMG_URL_MAP = [
   {
-    url: '/static/cq/cq_1.JPG'
+    url: '../static/cq/cq_1.JPG'
   },
   {
-    url: '/static/cq/cq_2.JPG'
+    url: '../static/cq/cq_2.JPG'
   },
   {
-    url: '/static/cq/cq_3.JPG'
+    url: '../static/cq/cq_3.JPG'
   },
   {
-    url: '/static/cq/cq_4.JPG'
+    url: '../static/cq/cq_4.JPG'
   },
   {
-    url: '/static/cq/cq_5.JPG'
+    url: '../static/cq/cq_5.JPG'
   }
 ]
 
